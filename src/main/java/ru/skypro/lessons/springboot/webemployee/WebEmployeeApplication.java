934b8f7501331f94aeede3179ea1f4f276ed4094
Nosach Liliya <mooduck@icloud.com>
2023-05-28 16:55:10 +0600

    Initial commit

---
+++ src/main/java/ru/skypro/lessons/springboot/webemployee/WebEmployeeApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.webemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/lessons/springboot/webemployee/WebEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.webemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
